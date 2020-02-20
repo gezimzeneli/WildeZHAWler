f685eb78a04d8468242ef61daa57a9480380b595
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2020-02-20 20:15:54 +0100

    Rollback

--- src/Main.java
+++ src/Main.java
@@ -71,7 +71,7 @@ public class Main {
     }
 
     private static void read(){
-        String file = "e_so_many_books.txt";
+        String file = "a_example.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             String[] items;
