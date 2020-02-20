29bd486664d5c9e3e0684bd7d3a6558a86ec9a6e
Stefan Bösch <stefan.boesch@zuehlke.com>
2020-02-20 20:02:27 +0100

    Update Writer.java

--- src/Writer.java
+++ src/Writer.java
@@ -2,6 +2,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Writer {
 
@@ -15,7 +16,7 @@ private BufferedWriter bufferedWriter;
         }
     }
 
-    public void write(String string){
+    private void write(String string){
         try {
             bufferedWriter.append(string);
         } catch (IOException e) {
@@ -23,11 +24,35 @@ private BufferedWriter bufferedWriter;
         }
     }
 
-    public void close(){
+    private void newLine(){
+        write("\n");
+    }
+
+    private void close(){
         try {
             bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void writeResult(ArrayList<Library> libraries){
+        write(String.valueOf(libraries.size()));
+        newLine();
+        for (Library library : libraries){
+            write(String.valueOf(library.getId()));
+            write("");
+            write(String.valueOf(library.getBocks().size()));
+
+            newLine();
+
+            for (Bock book : library.getBocks()){
+                write(String.valueOf(book.getId()));
+                write("");
+            }
+            newLine();
+
+        }
+        close();
     }
 }
