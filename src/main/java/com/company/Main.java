53ba245c8bbbf8d7f5ef52734dc03e09fa01f30e
Gezim Zeneli <g_zeneli@hotmail.com>
2021-02-23 21:30:06 +0100

    Project Setup

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Was luegsh?!?!");
+        System.err.println("Ich meins ernst!!!");
+    }
+}
