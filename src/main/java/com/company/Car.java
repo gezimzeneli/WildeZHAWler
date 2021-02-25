f38d2357ff5efff6f495ae2051d696e599bbea7b
Gezim Zeneli <g_zeneli@hotmail.com>
2021-02-25 19:45:03 +0100

    getter setter added

--- src/main/java/com/company/Car.java
+++ src/main/java/com/company/Car.java
@@ -5,4 +5,21 @@ import java.util.Queue;
 public class Car {
     long id;
     Queue<Street> route;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Queue<Street> getRoute() {
+        return route;
+    }
+
+    public void setRoute(Queue<Street> route) {
+        this.route = route;
+    }
 }
--- src/main/java/com/company/Intersection.java
+++ src/main/java/com/company/Intersection.java
@@ -12,5 +12,35 @@ public class Intersection {
     Map<Street, Integer> schedule = new HashMap<>();
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Street> getInput() {
+        return input;
+    }
+
+    public void setInput(List<Street> input) {
+        this.input = input;
+    }
+
+    public List<Street> getOutput() {
+        return output;
+    }
+
+    public void setOutput(List<Street> output) {
+        this.output = output;
+    }
+
+    public Map<Street, Integer> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Map<Street, Integer> schedule) {
+        this.schedule = schedule;
+    }
 }
