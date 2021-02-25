1c49840c300c817b1cd91440d0e228ca35384094
Gezim Zeneli <g_zeneli@hotmail.com>
2021-02-25 19:47:29 +0100

    Create Simulation.java

---
+++ src/main/java/com/company/Simulation.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Simulation {
+    int duration;
+
+
+    
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
